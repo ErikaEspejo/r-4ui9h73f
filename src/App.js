8e733257ac0bcfe68a1afbf0248fabbcb1be89a6
Erika Espejo <erikaespejo95@gmail.com>
2021-07-04 14:53:41 -0500

    repetidor

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({ word: event.target.value });
+  };
   render() {
+    const { word } = this.state;
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input
+          type="text"
+          placeholder="Empieza a escribir algo"
+          onChange={this.handleChange}
+        />
+        <p className="repeater">{word}</p>
       </div>
     );
   }
